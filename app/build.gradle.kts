1014e822396fa38e4cee7b4be487060901ec745c
kiran <kiran.211720@gmail.com>
2024-06-04 00:26:04 +0545

    added register,login and welcome activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
